effbc6ed3d65556d7e5842291dc754b3b45375a5
yukuku <yukuku@26a29d3b-5445-0410-962b-f982051ab157>
2008-02-04 19:07:13 +0000

    add class Guest

---
+++ wedding2/src/sg/edu/ntu/wedding/Guest.java
@@ -0,0 +1,135 @@
+package sg.edu.ntu.wedding;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class Guest {
+	private String name;
+	private boolean allocated;
+	private int tableID;
+	private String category;
+	private String invitedBy;
+	private String status;
+	private int guestTotal;
+	private int guestVeg;
+	private int guestMus;
+
+	public Guest() {
+	}
+
+    public Guest(ResultSet rs) throws SQLException {
+        rs.next();
+        name = rs.getString("name");
+        allocated = rs.getBoolean("allocated");
+        tableID = rs.getInt("tableID");
+        category = rs.getString("category");
+        invitedBy = rs.getString("invitedBy");
+        status = rs.getString("status");
+        guestTotal = rs.getInt("guestTotal");
+        guestVeg = rs.getInt("guestVeg");
+        guestMus = rs.getInt("guestMus");
+    }
+
+    public Guest(HttpServletRequest req) {
+        name = req.getParameter("name");
+        allocated = parseBoolean(req.getParameter("allocated"));
+        tableID = parseInt(req.getParameter("tableID"));
+        category = req.getParameter("category");
+        invitedBy = req.getParameter("invitedBy");
+        status = req.getParameter("status");
+        guestTotal = parseInt(req.getParameter("guestTotal"));
+        guestVeg = parseInt(req.getParameter("guestVeg"));
+        guestMus = parseInt(req.getParameter("guestMus"));    
+    }
+
+    private static int parseInt(String s) {
+    	try {
+    		return Integer.parseInt(s);
+    	} catch (NumberFormatException e) {
+    		return 0;
+    	}
+    }
+    private static boolean parseBoolean(String s) {
+    	try {
+    		return Integer.parseInt(s) != 0;
+    	} catch (NumberFormatException e) {
+    		return Boolean.parseBoolean(s);
+    	}
+    }
+    
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public boolean isAllocated() {
+		return allocated;
+	}
+
+	public void setAllocated(boolean allocated) {
+		this.allocated = allocated;
+	}
+
+	public int getTableID() {
+		return tableID;
+	}
+
+	public void setTableID(int tableID) {
+		this.tableID = tableID;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public String getInvitedBy() {
+		return invitedBy;
+	}
+
+	public void setInvitedBy(String invitedBy) {
+		this.invitedBy = invitedBy;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public int getGuestTotal() {
+		return guestTotal;
+	}
+
+	public void setGuestTotal(int guestTotal) {
+		this.guestTotal = guestTotal;
+	}
+
+	public int getGuestVeg() {
+		return guestVeg;
+	}
+
+	public void setGuestVeg(int guestVeg) {
+		this.guestVeg = guestVeg;
+	}
+
+	public int getGuestMus() {
+		return guestMus;
+	}
+
+	public void setGuestMus(int guestMus) {
+		this.guestMus = guestMus;
+	}
+    
+    
+}
