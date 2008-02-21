1e14767c32eb66733ce418029d4dd9f71a10e8fd
yukuku <yukuku@26a29d3b-5445-0410-962b-f982051ab157>
2008-02-21 20:51:52 +0000

    bug fix Guest: tableId->tableNumber, add getId();

--- trunk/wedding2/src/sg/edu/ntu/wedding/Guest.java
+++ trunk/wedding2/src/sg/edu/ntu/wedding/Guest.java
@@ -8,39 +8,45 @@ import javax.servlet.http.HttpServletRequest;
 public class Guest {
 	private String name;
 	private boolean allocated;
-	private int tableID;
+	private int tableNumber;
 	private String category;
 	private String invitedBy;
 	private String status;
 	private int guestTotal;
 	private int guestVeg;
 	private int guestMus;
-
+	private int id;
+	
 	public Guest() {
 	}
 
     public Guest(ResultSet rs) throws SQLException {
         name = rs.getString("name");
         allocated = rs.getBoolean("allocated");
-        tableID = rs.getInt("tableID");
+        tableNumber = rs.getInt("tableNumber");
         category = rs.getString("category");
         invitedBy = rs.getString("invitedBy");
         status = rs.getString("status");
         guestTotal = rs.getInt("guestTotal");
         guestVeg = rs.getInt("guestVeg");
         guestMus = rs.getInt("guestMus");
+        id = rs.getInt("id");
     }
 
     public Guest(HttpServletRequest req) {
         name = req.getParameter("name");
         allocated = Parse.toBoolean(req.getParameter("allocated"));
-        tableID = Parse.toInt(req.getParameter("tableID"));
+        tableNumber = Parse.toInt(req.getParameter("tableNumber"));
         category = req.getParameter("category");
         invitedBy = req.getParameter("invitedBy");
         status = req.getParameter("status");
         guestTotal = Parse.toInt(req.getParameter("guestTotal"));
         guestVeg = Parse.toInt(req.getParameter("guestVeg"));
         guestMus = Parse.toInt(req.getParameter("guestMus"));    
+    }
+    
+    public int getId() {
+    	return id;
     }
 
 	public String getName() {
@@ -59,12 +65,12 @@ public class Guest {
 		this.allocated = allocated;
 	}
 
-	public int getTableID() {
-		return tableID;
+	public int getTableNumber() {
+		return tableNumber;
 	}
 
-	public void setTableID(int tableID) {
-		this.tableID = tableID;
+	public void setTableNumber(int tableNumber) {
+		this.tableNumber = tableNumber;
 	}
 
 	public String getCategory() {
