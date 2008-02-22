0a2c2b198823031a6b7dc910e0ef34d4fcb151cf
xushilei <xushilei@26a29d3b-5445-0410-962b-f982051ab157>
2008-02-22 13:58:49 +0000

    adjust side menu bar to leave space for top banner.

--- wedding2/WebContent/js/menu_tpl.js
+++ wedding2/WebContent/js/menu_tpl.js
@@ -7,7 +7,7 @@ var MENU_POS = [
 	'width': 140,
 	// absolute position of the menu on the page (in pixels)
 	// with centered content use Tigra Menu PRO or Tigra Menu GOLD
-	'block_top': 15,
+	'block_top': 90,
 	'block_left': 20,
 	// offsets between items of the same level (in pixels)
 	'top': 24,
