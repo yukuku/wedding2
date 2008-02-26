03b2dd28ae71d0b785108c01e9aa225b5125b877
xushilei <xushilei@26a29d3b-5445-0410-962b-f982051ab157>
2008-02-26 09:54:45 +0000

    delete guest

--- wedding2/src/sg/edu/ntu/wedding/Constant.java
+++ wedding2/src/sg/edu/ntu/wedding/Constant.java
@@ -4,6 +4,7 @@ public class Constant {
     public class Session {
         public static final String activeWedding = "activeWedding";
         public static final String guestTableQry = "SELECT * FROM IP_GUEST WHERE WEDDINGID=?";
+        public static final String guestTableQry2 = "SELECT * FROM IP_GUEST WHERE ID=?";
         public static final String weddingTableQryAll = "SELECT * FROM IP_WEDDING";
         public static final String guestTableSqs = "Guest Title, Invited By, Table ID, Guest Number, Vegetarians, Muslims";
     }
