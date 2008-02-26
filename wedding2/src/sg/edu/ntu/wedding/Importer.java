2c11b860deaf21b86f4cdef17fe77099df3ca84c
jeremy.teo.w.m <jeremy.teo.w.m@26a29d3b-5445-0410-962b-f982051ab157>
2008-02-26 13:48:52 +0000

    Importer - With Auto-Assign Function!

--- wedding2/src/sg/edu/ntu/wedding/Importer.java
+++ wedding2/src/sg/edu/ntu/wedding/Importer.java
@@ -15,43 +15,42 @@ public class Importer {
 	private static boolean blnValidation;
 	private static DatabaseConnection dbcConnection = new DatabaseConnection();
 	
-	public static int processFile(int intWeddingId, InputStreamReader strFileName) throws Exception {
+	public static int processFile(int intWeddingId, InputStreamReader strFileName, int intAutoAssign, Wedding wWedding) throws Exception {
 
 		// Vector<Vector<String>> vFileContents = CSVFile.read(new File(new Scanner(strFileName).nextLine()));
 	    Vector<Vector<String>> vFileContents = CSVFile.read(strFileName);
-		blnValidation = false;
 		
 		for (Vector<String> vFileContent : vFileContents) {
 			if (vFileContent.size() == 6) {
 				// Validate File Name
 				String strName = vFileContent.elementAt(0).trim();
 				if (strName.length() == 0 || strName.length() > 80)
-					return 2;
+					return -1;
 				
 				// Validate Category
 				String strCategory = vFileContent.elementAt(1).trim();
 				if (strCategory.compareTo("relative") != 0 && strCategory.compareTo("collague") != 0 && strCategory.compareTo("friend") != 0)
-					return 3;
+					return -2;
 				
 				// Validate InvitedBy
 				String strInvitedBy = vFileContent.elementAt(2).trim();
 				if (strInvitedBy.compareTo("groom") != 0 && strInvitedBy.compareTo("bride") != 0 && strInvitedBy.compareTo("both") != 0)
-					return 4;
+					return -3;
 
 				// Validate Guest Total
 				String strGuestTotal = vFileContent.elementAt(3).trim();
 				if (validateNumber(strGuestTotal) == false)
-					return 5;
+					return -4;
 				
 				// Validate Guest Vegetarian
 				String strGuestVegetarian = vFileContent.elementAt(4).trim();
 				if (validateNumber(strGuestVegetarian) == false)
-					return 6;
+					return -5;
 				
 				// Validate Guest Muslim
 				String strGuestMuslim = vFileContent.elementAt(5).trim();
 				if (validateNumber(strGuestMuslim) == false)
-					return 7;
+					return -6;
 				
 				// Validate that Guest Total is always more or at least equal to the sum
 				// of Guest Vegetarian and Guest Muslim
@@ -60,27 +59,38 @@ public class Importer {
 				int intGuestMuslim = new Integer (strGuestMuslim);
 				
 				if (intGuestTotal < (intGuestVegetarian + intGuestMuslim))
-					return 8;
+					return -7;
 			}
 			else {
-				return 9;
+				return -8;
 			}
-			blnValidation = true;
 		}	
-		
-		
-		if (blnValidation) {
+		if (intAutoAssign == 1) {
+			int intAutoAssignResult;
+			boolean blnAutoAssignNotComplete = false;
 			// Prepare for Import only if all rows are validated correctly
 			for (Vector<String> vFileContent : vFileContents) {
-				dbcConnection.insert("Insert Into IP_GUEST (weddingID, name, category, invitedBy, status, guestTotal, guestVeg, guestMus) values (?, ?, ?, ?, ?, ?, ?, ?)", 
+				int intGuestId = dbcConnection.insert("Insert Into IP_GUEST (weddingID, name, category, invitedBy, status, guestTotal, guestVeg, guestMus) values (?, ?, ?, ?, ?, ?, ?, ?)", 
 				intWeddingId, vFileContent.elementAt(0).trim(), vFileContent.elementAt(1).trim(), vFileContent.elementAt(2).trim(), "invited", vFileContent.elementAt(3).trim(), vFileContent.elementAt(4).trim(), vFileContent.elementAt(5).trim());
+				if (intAutoAssign == 1) {
+					intAutoAssignResult = AutoAssign.AutoAssignSingleGuest(intGuestId, wWedding.getId(), Integer.parseInt(vFileContent.elementAt(3).trim()), Integer.parseInt(vFileContent.elementAt(4).trim()), Integer.parseInt(vFileContent.elementAt(5).trim()), wWedding);	
+					if (intAutoAssignResult < 1)
+						blnAutoAssignNotComplete = true;
+				}
 			}
-			return 1;
+			if (blnAutoAssignNotComplete == false)
+				return 1;
+			else
+				return 2;
 		}
 		else {
-			return 0;
+			for (Vector<String> vFileContent : vFileContents) {
+				int intGuestId = dbcConnection.insert("Insert Into IP_GUEST (weddingID, name, category, invitedBy, status, guestTotal, guestVeg, guestMus) values (?, ?, ?, ?, ?, ?, ?, ?)", 
+				intWeddingId, vFileContent.elementAt(0).trim(), vFileContent.elementAt(1).trim(), vFileContent.elementAt(2).trim(), "invited", vFileContent.elementAt(3).trim(), vFileContent.elementAt(4).trim(), vFileContent.elementAt(5).trim());
+			}	
+			return 1;
 		}
-		
+
 	}
 	
 	private static boolean validateNumber (String strVariable) {
