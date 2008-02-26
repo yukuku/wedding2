af855d4196e7a87167e11e74cad2302588ad74a3
jeremy.teo.w.m <jeremy.teo.w.m@26a29d3b-5445-0410-962b-f982051ab157>
2008-02-26 12:34:54 +0000

    AutoAssign - SingleGuest With Check Table Type Done!

--- wedding2/src/sg/edu/ntu/wedding/AutoAssign.java
+++ wedding2/src/sg/edu/ntu/wedding/AutoAssign.java
@@ -12,20 +12,38 @@ public class AutoAssign {
 	private static boolean blnAssigned;
 	private static DatabaseConnection dbcConnection = new DatabaseConnection();
 	
-	public static int AutoAssignSingleGuest (int intGuestId, int intWeddingId, int intTotalGuests) throws Exception  {
+	public static int AutoAssignSingleGuest (int intGuestId, int intWeddingId, int intTotalGuests, int intTotalVeg, int intTotalMus, Wedding wWedding) throws Exception  {
+		Table.Type tblType;
 		ResultSet rsWeddingTables = dbcConnection.select("Select * From IP_TABLE Where weddingID = ? And vacancy > 0 Order By vacancy, number", intWeddingId);
 		if (rsWeddingTables.last() == true) {
+			
+			// Determine the type of table for the current guest
+			if ((intTotalGuests > 0 && intTotalVeg > 0 && intTotalMus > 0) || (intTotalGuests > 0 && intTotalVeg == 0 && intTotalMus > 0) || (intTotalGuests > 0 && intTotalVeg > 0 && intTotalMus == 0)) {
+				if (intTotalGuests == intTotalVeg)
+					tblType = Table.Type.VEG;
+				else if (intTotalGuests == intTotalMus)
+					tblType = Table.Type.MUS;
+				else
+					tblType = Table.Type.MIXED;
+			}
+			else if (intTotalGuests ==0 && intTotalVeg > 0 && intTotalMus == 0)
+				tblType = Table.Type.VEG;
+			else
+				tblType = Table.Type.MUS;
+			
 			rsWeddingTables.first();
 			do  {
-				if (rsWeddingTables.getInt("vacancy") >= intTotalGuests) {
-					dbcConnection.execute("Update IP_GUEST Set tableNumber = ? Where id=?", rsWeddingTables.getInt("number"), intGuestId);
-					dbcConnection.execute("Update IP_TABLE Set vacancy = vacancy - ? Where weddingID=? And number = ?", intTotalGuests, intWeddingId, rsWeddingTables.getInt("number"));
-					// Return 1 if auto assignment for the current guest is successful
-					return rsWeddingTables.getInt("number");
-				}
+					Table tblTable = new Table(rsWeddingTables);
+					tblTable.detectType(dbcConnection, wWedding);
+					if ((tblTable.getType() == Table.Type.UNKNOWN || tblTable.getType() == tblType) && rsWeddingTables.getInt("vacancy") >= intTotalGuests) {
+						dbcConnection.execute("Update IP_GUEST Set tableNumber = ? Where id=?", rsWeddingTables.getInt("number"), intGuestId);
+						dbcConnection.execute("Update IP_TABLE Set vacancy = vacancy - ? Where weddingID=? And number = ?", intTotalGuests, intWeddingId, rsWeddingTables.getInt("number"));
+						// Return 1 if auto assignment for the current guest is successful
+						return rsWeddingTables.getInt("number");
+					}
 			}
 			while(rsWeddingTables.next());
-			// Return 3 if there are no tables with adequate vacancies
+			// Return 3 if there are no tables with adequate vacancies or type
 			return -3;
 		}
 		else
