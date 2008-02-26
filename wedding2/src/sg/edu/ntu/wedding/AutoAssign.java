26db3a2bfdd66375c3bd599a7c80bfc16adbb27c
jeremy.teo.w.m <jeremy.teo.w.m@26a29d3b-5445-0410-962b-f982051ab157>
2008-02-26 11:05:23 +0000

    AutoAssign-Assign Single Guest Completed

--- wedding2/src/sg/edu/ntu/wedding/AutoAssign.java
+++ wedding2/src/sg/edu/ntu/wedding/AutoAssign.java
@@ -9,28 +9,28 @@ import java.util.Scanner;
 import java.util.Vector;
 
 public class AutoAssign {
-	private static boolean blnValidation;
+	private static boolean blnAssigned;
 	private static DatabaseConnection dbcConnection = new DatabaseConnection();
 	
 	public static int AutoAssignSingleGuest (int intGuestId, int intWeddingId, int intTotalGuests) throws Exception  {
 		ResultSet rsWeddingTables = dbcConnection.select("Select * From IP_TABLE Where weddingID = ? And vacancy > 0 Order By vacancy, number", intWeddingId);
-		
-		if (rsWeddingTables.getFetchSize() > 0) {
-			while(rsWeddingTables.next()) {
+		if (rsWeddingTables.last() == true) {
+			rsWeddingTables.first();
+			do  {
 				if (rsWeddingTables.getInt("vacancy") >= intTotalGuests) {
 					dbcConnection.execute("Update IP_GUEST Set tableNumber = ? Where id=?", rsWeddingTables.getInt("number"), intGuestId);
 					dbcConnection.execute("Update IP_TABLE Set vacancy = vacancy - ? Where weddingID=? And number = ?", intTotalGuests, intWeddingId, rsWeddingTables.getInt("number"));
 					// Return 1 if auto assignment for the current guest is successful
-					return 1;
+					return rsWeddingTables.getInt("number");
 				}
 			}
+			while(rsWeddingTables.next());
 			// Return 3 if there are no tables with adequate vacancies
-			return 3;
+			return -3;
 		}
-		else {
-			// Return 2 if there are no tables for the current wedding
-			return 2;
-		}
+		else
+			return -2;
+		
 	}
 	
 	public static int AutoAssignAllGuests () {
